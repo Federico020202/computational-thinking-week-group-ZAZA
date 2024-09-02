f09a605712151e45e0215cfd6fb88ee3e9c7fe1b
tokyonjams <169032740+tokyonjams@users.noreply.github.com>
2024-09-02 12:51:15 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,8 +1,9 @@
 from clem.py import return_clem_name
 from akira.py import return_akira_name
 from Porter.py import return_clem_name
+from fede.py import return_fede_name 
 
-print("this is team zaza. We are: " + return_clem_name + return_Porter_name + return_akira_name)
+print("this is team zaza. We are: " + return_clem_name + return_Porter_name + return_akira_name + return_fede_name)
 
 
 
