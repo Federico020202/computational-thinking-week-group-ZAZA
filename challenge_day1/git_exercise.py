5f6db7d4e39bde143feba8a9c9f1e5092269b200
clementpaitrault <clement.paitrault@student.uva.nl>
2024-09-02 12:59:29 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,8 +2,9 @@ from clem import return_clem_name
 from akira import return_akira_name
 from Porter import return_Porter_name
 from fede import display_fede 
+from Joeben import get_name
 
-print("This is team Zaza. We are: " + return_clem_name() + ", " + return_Porter_name() + ", " + return_akira_name() + ", " + display_fede())
+print("This is team Zaza. We are: " + return_clem_name() + ", " + return_Porter_name() + ", " + return_akira_name() + ", " + display_fede() + ", " + get_name())
 
 
 
