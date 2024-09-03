6b4010b604fe22687ff5c17a2aa83daea39cf91c
clementpaitrault <clement.paitrault@student.uva.nl>
2024-09-03 10:36:04 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,23 +1,27 @@
 correspondance_dict = {
-  a:3,
-  b:-1,
-  c:4,
-  d:7,
-  e:0.5
+  "a":3,
+  "b":-1,
+  "c":4,
+  "d":7,
+  "e":0.5
 }
 
 def solution_station_7(input):
   input = str(input) 
   result = []
-  for char in input
+  for char in input:
     is_in_dict = char in correspondance_dict
     if is_in_dict == True: 
-      result.append(correspondance_dict(char))
+      result.append(str(correspondance_dict[char]))
     else:
-      result.append(char)
+      result.append(str(char))
 
-  solution = eval(result)
+  result_list = "".join(result)
+  print(result_list)
+  solution = eval(str(result_list))
 
   return solution
   
+
+
 
